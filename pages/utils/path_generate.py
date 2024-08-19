0de3be151ced86edefee321b63110e709acde3fe
Alexander Ischenko <ischeenko@gmail.com>
2024-08-19 17:34:44 +0300

    Add new branch 'mid-level-step-object' with regirstration_page.

--- pages/utils/path_generate.py
+++ pages/utils/path_generate.py
@@ -3,5 +3,5 @@ import resources
 
 def generate_path(file_name):
         return  str(
-        Path(resources.__file__).parent.joinpath(f'resources/{file_name}').absolute()
-    )
+        Path(resources.__file__).parent.parent.joinpath(f'resources/'
+                                                        f'{file_name}'))
